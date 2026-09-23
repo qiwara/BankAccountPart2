66254f590accb28e9b712704df7d3f0c9ea9791d
Qi <qye2@charlotte.edu>
2026-09-23 12:05:05 -0400

    fix: checking withdraw test works with minimum balance checks

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ savingsAcct2 = Savings("Qi", 0.003)
 # Initial account setup
 checkingAcct1 = Checking("Qi", 1000)
 checkingAcct2 = Checking("Parth", 1000)
-checkingAcct1.deposit(2000)
+checkingAcct1.deposit(200)
 checkingAcct2.deposit(1000)
 checkingAcct1.print_customer_information()
 checkingAcct2.print_customer_information()
 
 # Transfer limit checks
-checkingAcct1.transfer(1001, checkingAcct2)
-checkingAcct1.transfer(1901, checkingAcct2)
+checkingAcct1.transfer(2000, checkingAcct2)
+checkingAcct1.transfer(200, checkingAcct2)
 checkingAcct1.transfer(100, checkingAcct2)
 
 # Transfer check
