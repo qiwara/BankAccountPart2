cdc3e75c7626ee4584fa90235657c5078f811ef9
Qi <qye2@charlotte.edu>
2026-09-23 11:57:38 -0400

    feat: main file created alongside 2 checking instances for testing

---
+++ main.py
@@ -0,0 +1,29 @@
+from bank import CoolBank
+from checking import Checking
+from savings import Savings
+
+savingsAcct1 = Savings("Parth", 0.005)
+savingsAcct2 = Savings("Qi", 0.003)
+
+# Initial account setup
+checkingAcct1 = Checking("Qi", 1000)
+checkingAcct2 = Checking("Parth", 1000)
+checkingAcct1.deposit(2000)
+checkingAcct2.deposit(1000)
+checkingAcct1.print_customer_information()
+checkingAcct2.print_customer_information()
+
+# Transfer limit checks
+checkingAcct1.transfer(1001, checkingAcct2)
+checkingAcct1.transfer(1901, checkingAcct2)
+checkingAcct1.transfer(100, checkingAcct2)
+
+# Transfer check
+checkingAcct1.print_customer_information()
+checkingAcct2.print_customer_information()
+
+# Withdraw check
+checkingAcct2.withdraw(1001)
+checkingAcct2.withdraw(100)
+checkingAcct2.print_customer_information()
+
